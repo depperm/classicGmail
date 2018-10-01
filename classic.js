39432e86127097da081128539bd21b8a30744535
Marshall <Epper.Marshall@hughes.com>
2018-10-01 16:32:23 -0400

    starting button grouping, adv color select

--- classic.js
+++ classic.js
@@ -1,11 +1,5 @@
 window.onload = function () {
-    //document.getElementsByClassName("T-I J-J5-Ji T-I-KE L3")[0].innerHTML="Compose";
-    //iframe
-    /*var cssLink = document.createElement("link");
-    cssLink.href = "classic.css"; 
-    cssLink.rel = "stylesheet"; 
-    cssLink.type = "text/css";
-    document.querySelectorAll("*[id^=wblh0]")[0].document.head.appendChild(cssLink);*/
+  
     //doesn't work-try to get rid of right side panel
     //document.getElementsByClassName("aT5-aOt-I brC-dA-I")[0].click();
     //change gmail logo
@@ -22,15 +16,21 @@ window.onload = function () {
 		compose: true,
 		buttons:true,
 		shadow:true,
-		lefticons:true
+		lefticons:true,
+        tabs:true,
+        composeBGColor:'d14836',
+        composeColor:'rgb(255,255,255)'
     }, function(items) {
 		if(items.everything){
-			//loadCSS('classic');
 			loadCSS('shadow');
 			loadCSS('compose');
 			loadCSS('lefticons');
 			loadCSS('buttons');
 			loadCSS('tabs');
+            
+            if(items.composeBGColor!='d14836'){
+                loadAdvCSS(['.T-I.J-J5-Ji.T-I-KE.L3{ color:'+items.composeColor+' !important;background-color:#'+items.composeBGColor+' !important;}','.aim{ border-left-color:#'+items.composeBGColor+' !important;}','.ain a{ color:#'+items.composeBGColor+' !important;}'])
+            }
 		}else{
 			if(items.shadow)
 				loadCSS('shadow');
@@ -40,11 +40,13 @@ window.onload = function () {
 				loadCSS('buttons');
 			if(items.lefticons)
 				loadCSS('lefticons');
+			if(items.tabs)
+				loadCSS('tabs');
 		}
     });
     console.log('done');
 }
-//loadCSS('classic');
+
 function loadCSS(file) {
     console.log('loading:'+file);
     var link = document.createElement("link");
@@ -53,6 +55,21 @@ function loadCSS(file) {
     link.type = "text/css";
     link.rel = "stylesheet";
     document.getElementsByTagName("html")[0].appendChild(link);
+}
+//referenced: http://jonraasch.com/blog/javascript-style-node
+function loadAdvCSS(rules){
+    var css = document.createElement('style');
+    css.type='text/css';
+    
+    var styles='';
+    $.each(rules,function(index,rule){
+        styles += rule;
+    });
+    
+    if (css.styleSheet) css.styleSheet.cssText = styles;
+    else css.appendChild(document.createTextNode(styles));
+    
+    document.getElementsByTagName("html")[0].appendChild(css);
 }
 function unloadCSS(file) {
     console.log('unloading');
--- options.js
+++ options.js
@@ -1,6 +1,6 @@
 let num_options=4;
 $(function() {
-	//hi();
+	//simply checks the checkboxes appropriately
 	function check_checkboxes(){
 		console.log('here2');
 		if($(this).attr('id')=='everything' && $(this).is(':checked')){
@@ -16,8 +16,51 @@ $(function() {
 		}
 	}
 	$('.classic-gmail-checkbox').on('click',check_checkboxes);
+    $('#advbtn').on('click',function(){
+        $('#advancedparameters').toggle();
+        if($('#advancedparameters').is(':visible'))
+            $('#advbtn').val("Hide Advanced Parameters");
+        else
+            $('#advbtn').val("Show Advanced Parameters");
+    });
+    
+    var colorpicker=false;
+    $('#composecolor').on('click',function(){
+        console.log('clicked');
+        if(!colorpicker){
+            var t=$('#composecolor').css('background-color');
+            t=t.slice(4,t.length-1);
+            t=t.split(',');
+            console.log(rgbToHex(parseInt(t[0]),parseInt(t[1]),parseInt(t[2])).slice(1));
+            //document.getElementById('color-picker').value=rgbToHex(parseInt(t[0]),parseInt(t[1]),parseInt(t[2])).slice(1);
+            //$('#color-picker').val(rgbToHex(parseInt(t[0]),parseInt(t[1]),parseInt(t[2])).slice(1));
+            $('#color-picker').attr('value',rgbToHex(parseInt(t[0]),parseInt(t[1]),parseInt(t[2])).slice(1));
+            //$('#color-picker').css('background-color',$('#composecolor').css('background-color'));
+            document.getElementById('color-picker').jscolor.show();
+        }else{
+            document.getElementById('color-picker').jscolor.hide();
+            $('#color-picker').toggleClass('jscolor');
+        }
+        colorpicker=!colorpicker;
+        //onclick="document.getElementById('color-picker').jscolor.show()"
+    });
+    $('#color-picker').on('change',function(){
+        $('#composecolor').css('background-color','#'+$(this).val());
+        $('#composecolor').css('color',$(this).css('color'));
+    });
+    $('#composedefault').on('click',function(){
+        $('#color-picker').attr('value','d14836');
+        $('#composecolor').css('background-color','#d14836');
+        $('#composecolor').css('color','white');
+    });
 });
-
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
 // Saves options to chrome.storage
 function save_options() {
     var everything = document.getElementById('everything').checked;
@@ -25,13 +68,20 @@ function save_options() {
     var buttons = document.getElementById('buttons').checked;
     var shadow = document.getElementById('shadow').checked;
     var lefticons = document.getElementById('lefticons').checked;
+    var tabs = document.getElementById('tabs').checked;
+    
+    var composeBGColor = $('#color-picker').val();
+    var composeColor = $('#color-picker').css('color');
 	
     chrome.storage.sync.set({
 		everything: everything,
 		compose: compose,
 		buttons:buttons,
 		shadow:shadow,
-		lefticons:lefticons
+		lefticons:lefticons,
+        tabs:tabs,
+        composeBGColor:composeBGColor,
+        composeColor:composeColor
     }, function() {
 		// Update status to let user know options were saved.
 		var status = document.getElementById('status');
@@ -45,19 +95,28 @@ function save_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-    // Use default value color = 'red' and likesColor = true.
     chrome.storage.sync.get({
 		everything: true,
 		compose: true,
 		buttons:true,
 		shadow:true,
-		lefticons:true
+		lefticons:true,
+        tabs:true,
+        composeBGColor:'d14836',
+        composeColor:'rgb(255,255,255)'
     }, function(items) {
 		document.getElementById('everything').checked = items.everything;
 		document.getElementById('compose').checked = items.compose;
 		document.getElementById('buttons').checked = items.buttons;
 		document.getElementById('shadow').checked = items.shadow;
 		document.getElementById('lefticons').checked = items.lefticons;
+		document.getElementById('tabs').checked = items.tabs;
+        
+        document.getElementById('composecolor').value = items.composeBGColor;
+        $('#composecolor').css('background-color','#'+items.composeBGColor);
+        $('#composecolor').css('color',items.composeColor);
+        //document.getElementById('composecolor').style.backgroundColor = items.composeBGColor;
+        //document.getElementById('composecolor').style.color = items.composeColor;
     });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
